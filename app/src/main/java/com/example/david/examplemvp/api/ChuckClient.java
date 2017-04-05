0abc3a0a8804ff5951b0893c9005662770f2519b
Davids89 <daviluqui@gmail.com>
2017-04-05 20:13:43 +0200

    Success API call

--- app/src/main/java/com/example/david/examplemvp/api/ChuckClient.java
+++ app/src/main/java/com/example/david/examplemvp/api/ChuckClient.java
@@ -1,5 +1,7 @@
 package com.example.david.examplemvp.api;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,9 +14,16 @@ public class ChuckClient {
     private static final String BASE_URL = "https://api.icndb.com/";
 
     public ChuckClient(){
+
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        client.addInterceptor(loggingInterceptor);
+
         this.retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client.build())
                 .build();
     }
 
--- app/src/main/java/com/example/david/examplemvp/api/ChuckService.java
+++ app/src/main/java/com/example/david/examplemvp/api/ChuckService.java
@@ -3,6 +3,7 @@ package com.example.david.examplemvp.api;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by david on 3/4/17.
@@ -10,6 +11,6 @@ import retrofit2.http.Path;
 
 public interface ChuckService {
     @GET("jokes/random")
-    Call<RandomJokeResponse> getRandom(@Path("firstName") String firstName,
-                                       @Path("lastName") String lastName);
+    Call<RandomJokeResponse> getRandom(@Query("firstName") String firstName,
+                                       @Query("lastName") String lastName);
 }
