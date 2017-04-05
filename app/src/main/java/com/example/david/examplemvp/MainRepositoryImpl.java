a1ebbb60524847f9257fbe47091ff00e2afe63ce
Davids89 <daviluqui@gmail.com>
2017-04-05 20:02:04 +0200

    New structure

--- app/src/main/java/com/example/david/examplemvp/MainRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.david.examplemvp;
-
-/**
- * Created by david on 3/4/17.
- */
-
-public class MainRepositoryImpl implements MainRepository {
-}
--- app/src/main/java/com/example/david/examplemvp/ui/MainView.java
+++
@@ -1,8 +0,0 @@
-package com.example.david.examplemvp.ui;
-
-/**
- * Created by david on 3/4/17.
- */
-
-public interface MainView {
-}
